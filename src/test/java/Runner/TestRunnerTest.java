dd7c87d74cb9f1d40216150b5d815ec62f71b26f
FebinaSharma <febinasharma16@gmail.com>
2024-08-23 15:31:05 -0400

    made changes to pom.xml and folder structure

---
+++ src/test/java/Runner/TestRunnerTest.java
@@ -0,0 +1,19 @@
+package Runner;
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features="C:/Users/febin/eclipse-workspace/CucumberPOM/src/test/resource/com/qa/features/homePage.feature",
+		glue= {"com.qa.stepDefinitions"},
+		dryRun=false,
+		monochrome = true,
+		plugin={"pretty","html:target/HtmlReports","json:target/JSONReports/report.json","junit:target/JUnitReports/report.xml"}
+		
+		
+		)
+public class TestRunnerTest {
+
+}
---
+++ src/test/java/com/qa/stepDefinitions/HomePageSteps.java
@@ -0,0 +1,45 @@
+package com.qa.stepDefinitions;
+
+import java.io.IOException;
+
+import org.junit.Assert;
+
+import com.qa.pages.ContactsPage;
+import com.qa.pages.HomePage;
+import com.qa.pages.LoginPage;
+import com.qa.util.TestBase;
+import com.qa.util.TestUtil;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class HomePageSteps extends TestBase{
+	LoginPage loginPg;
+	HomePage homePg;
+	ContactsPage contactsPg;
+	@Given("^User opens the browser and enter the url$")
+	public void user_opens_the_browser_and_enter_the_url(){
+		TestBase.initialize();
+	}
+
+	@When("^User is on the login page of the application$")
+	public void user_is_on_the_login_page_of_the_application(){
+	  loginPg=new LoginPage();
+	  Assert.assertEquals("Free CRM software for customer relationship management, sales, and support.", loginPg.validateLoginPageTitle());
+	 // System.out.println(loginPg);
+	}
+	
+	@Then("^User enters valid credentials and logs into appilcation$")
+	public void user_enters_valid_credentials_and_logs_into_appilcation() throws IOException {
+	homePg=loginPg.login(prop.getProperty("username"), prop.getProperty("password"));
+	TestUtil.takeScreenshotAtEndOfTest();
+	}
+
+	@Then("^User is navigated to Home Page$")
+	public void user_is_navigated_to_Home_Page(){
+		Assert.assertEquals("CRMPRO", homePg.verifyPageTitle());
+		
+	}
+
+}
